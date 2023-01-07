7c4e1dac32724aa417cdd13d3406f1e794bc54a9
VitaBabenko <babenkovita1988@gmail.com>
2023-01-07 17:41:41 +0200

    changed

---
+++ src/fetchCountries.js
@@ -0,0 +1,18 @@
+import Notiflix from 'notiflix';
+
+function fetchCountries(name) {
+    const BASE_URL = 'https://restcountries.com/v3.1/name';
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status === 404) {
+                    Notiflix.Notify.failure("Oops, there is no country with that name");
+                }
+                throw new Error(resp.status)
+            }
+
+            return resp.json()
+        })
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import {fetchCountries} from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(evt) {
+    const dataInput = evt.target.value.trim();
+    fetchCountries(dataInput)
+        .then(data => createMarkup(data))
+        .catch(err => console.log(err))
+}
+
+function createMarkup(obj) {
+    console.log(obj)
+
+}
