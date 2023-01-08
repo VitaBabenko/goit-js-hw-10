808ee4ed216fe00eae08d51bbcd4451a09dc2895
VitaBabenko <babenkovita1988@gmail.com>
2023-01-08 14:45:23 +0200

    done

--- src/index.js
+++ src/index.js
@@ -14,18 +14,43 @@ input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(evt) {
     const dataInput = evt.target.value.trim();
     if (dataInput === '') {
-        return
+        info.innerHTML = [];
+        list.innerHTML = [];
     } else {
         fetchCountries(dataInput)
-        .then(data => createMarkup(data))
-        .catch(err => console.log(err))
-
+            .then(data => {
+                if (data.length > 10) {
+                    Notiflix.Notify.failure("Too many matches found. Please enter a more specific name.");
+                    info.innerHTML = [];
+                    list.innerHTML = [];
+                } else if (data.length >= 2 && data.length <= 10) {
+                    createMarkupList(data)
+                } else if (data.length === 1) {
+                    createMarkupInfo(data)
+                }
+            })
+            .catch(err => console.log(err))
     }
-    
 }
 
-function createMarkup(obj) {
-    const markup = obj.map(({
+function createMarkupList(obj) {
+    const markupList = obj.map(({
+        flags: {
+            svg
+        },
+        name: {
+            official
+        } }) => `<li class="list-item">
+        <img src="${svg}" alt="${official}" width="30" height="20">
+        <h2 class="list-title">${official}</h2>
+        </li>`)
+    
+    list.innerHTML = markupList.join('');
+    info.innerHTML = [];
+}
+
+function createMarkupInfo(obj) {
+    const markupInfo = obj.map(({
         flags: {
             svg
         },
@@ -33,10 +58,20 @@ function createMarkup(obj) {
             official
         },
         capital,
-        languages: {
-            ukr
-        },
-        population
-    }) => ``)
+        population,
+        languages
+    }) => `<li>
+    <div class="info-container">
+    <img src="${svg}" alt="${official}" width="30" height="20">
+    <h2 class="info-title">${official}</h2></div>
+    <p>
+    <span class="info-span">Capital:</span> ${capital}</p>
+    <p>
+    <span class="info-span">Population:</span> ${population}</p>
+    <p>
+    <span class="info-span">Languages:</span> ${Object.values(languages)}</p>
+    </li>`)
 
+    info.innerHTML = markupInfo.join('');
+    list.innerHTML = [];
 }
